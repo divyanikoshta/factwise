fdc86b31b1c072d990b487f57d0464e2a6434354
Divyani Koshta <divyanikostha@gmail.com>
2022-09-25 19:48:15 +0530

    updated

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Celebrities from "./pages/Celebrities";
 
 const Routes = () => (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Celebrities}/>
+        <Route exact path="/" component={() => <Redirect to={"/celebrities"} />}/>
+        <Route exact path="/celebrities" component={Celebrities}/>
       </Switch>
     </BrowserRouter>
   );
